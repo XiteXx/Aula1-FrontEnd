25863e1ab556eb2a9df484ff53ee76c93e8b595c
XiteXx <monteirov@outlook.pt>
2026-05-06 20:36:33 +0100

    Criação de tags

---
+++ 06052026/src/app/tags-h/tags-h.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tags-h',
+  imports: [],
+  templateUrl: './tags-h.html',
+  styleUrl: './tags-h.css',
+})
+export class TagsH {
+  texto: string = "Leo";
+  textoArray: string[] = ["1st Pos", "2nd Pos", "3rd Pos"];
+}
